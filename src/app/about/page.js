ff7ed83b43750f9fee7607b598189e43a3d594c8
Tushar <tusharkumarsahu1998@gmail.com>
2024-05-22 10:18:53 +0530

    Final submission

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,9 +1,9 @@
 "use client";
-// import { ServiceData } from "@/lib/constants";
+import { ServiceData } from "@/lib/constants";
 import React from "react";
-// import AboutHero from "../../components/about/AboutHero";
-// import ServicesCard from "@/components/services/ServicesCard";
-// import OurTeam from "@/components/about/OurTeam";
+import AboutHero from "../../components/about/AboutHero";
+import ServicesCard from "@/components/services/ServicesCard";
+import OurTeam from "@/components/about/OurTeam";
 import useLight from "@/customhooks/useLight";
 import AboutHome from "@/components/about/AboutHome";
 import Services from "@/components/services/Services";
@@ -14,7 +14,7 @@ const About = () => {
     <div className="pt-24 mb-24">
       <AboutHome />
       <Services />
-      {/* <AboutHero />
+      <AboutHero />
             <div className="flex justify-center  py-12" >
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 px-4">
                     {ServiceData.map((item) => (
@@ -23,7 +23,7 @@ const About = () => {
                 </div>
             </div>
 
-            <OurTeam/> */}
+            <OurTeam/>
     </div>
   );
 };
